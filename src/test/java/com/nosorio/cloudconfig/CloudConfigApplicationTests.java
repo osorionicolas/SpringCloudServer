043b5b85f19ea45de8792f521c9a152f3cb1946a
Nicolas Osorio <osorionicolas95@gmail.com>
2020-07-25 01:32:00 -0300

    First commit

---
+++ src/test/java/com/nosorio/cloudconfig/CloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nosorio.cloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
